34e583186195d9843fa31ad1571b26bf2ee308a4
AmrutaZagade <157120314+AmrutaZagade@users.noreply.github.com>
2024-05-12 10:49:01 +0530

    Update move.js

--- components/Move/move.js
+++ components/Move/move.js
@@ -4,52 +4,44 @@ import { GlobalProps } from "../Redux/GlobalProps";
 import { setElement } from "../Redux/features/elementSlice";
 
 export const move = (event, elements) => {
-
     const selectedElement = store.getState().selectedElement.value;
-    if(selectedElement === null) {
+    if (selectedElement === null) {
         return;
     }
-    const { id, x1, x2, y1, y2, type, offSetX, offSetY, rectCoordinatesOffsetX, rectCoordinatesOffsetY, text } = selectedElement;
 
+    const { id, x1, x2, y1, y2, type, offSetX, offSetY, rectCoordinatesOffsetX, rectCoordinatesOffsetY, text, points } = selectedElement;
 
-    if (type != 'pencil') {
+    if (type !== 'pencil') {
         const width = x2 - x1;
         const height = y2 - y1;
-
         updateElement(id, event.clientX - offSetX, event.clientY - offSetY, event.clientX - offSetX + width, event.clientY - offSetY + height, type, { text: text });
     } else {
-
-
-        const newPoints = [];
-        selectedElement.points.forEach((_, index) => {
-
-            newPoints.push({
-                x: event.clientX - selectedElement.offSetX[index]
-
-                , y: event.clientY - selectedElement.offSetY[index]
-            })
-        });
+        const newPoints = points.map((point, index) => ({
+            x: event.clientX - offSetX[index],
+            y: event.clientY - offSetY[index]
+        }));
 
         let tempNewArray = [...elements];
-
-
         const width = x2 - x1;
         const height = y2 - y1;
         const newId = parseInt(id.split("#")[1]);
 
-
         tempNewArray[newId] = {
             ...tempNewArray[newId],
-            points: newPoints, x1: event.clientX - rectCoordinatesOffsetX, y1: event.clientY - rectCoordinatesOffsetY, x2: event.clientX - rectCoordinatesOffsetX + width, y2: event.clientY - rectCoordinatesOffsetY + height
+            points: newPoints,
+            x1: event.clientX - rectCoordinatesOffsetX,
+            y1: event.clientY - rectCoordinatesOffsetY,
+            x2: event.clientX - rectCoordinatesOffsetX + width,
+            y2: event.clientY - rectCoordinatesOffsetY + height
         };
 
-        store.dispatch(setElement([tempNewArray, true,id.split("#")[0]]));
-        const roomId =   GlobalProps.room;
-  if(roomId != null) {
-    tempNewArray = tempNewArray[newId];
-    const key = id.split("#")[0];
-    GlobalProps.socket.emit("render-elements", { tempNewArray, roomId,key });
-  }
-    }
+        store.dispatch(setElement([tempNewArray, true, id.split("#")[0]));
 
+        const roomId = GlobalProps.room;
+        if (roomId) {
+            const updatedElement = tempNewArray[newId];
+            const key = id.split("#")[0];
+            GlobalProps.socket.emit("render-elements", { updatedElement, roomId, key });
+        }
+    }
 }
