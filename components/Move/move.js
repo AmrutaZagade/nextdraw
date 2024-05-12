0bd2583e1ab3471c296092bde21a2ad095fc3251
AmrutaZagade <157120314+AmrutaZagade@users.noreply.github.com>
2024-05-12 12:47:08 +0530

    Update move1.js

--- components/Move/move.js
+++ components/Move/move.js
@@ -4,52 +4,69 @@ import { GlobalProps } from "../Redux/GlobalProps";
 import { setElement } from "../Redux/features/elementSlice";
 
 export const move = (event, elements) => {
-
     const selectedElement = store.getState().selectedElement.value;
-    if(selectedElement === null) {
+    if (!selectedElement) {
         return;
     }
-    const { id, x1, x2, y1, y2, type, offSetX, offSetY, rectCoordinatesOffsetX, rectCoordinatesOffsetY, text } = selectedElement;
 
+    const { id, x1, x2, y1, y2, type, offSetX, offSetY, rectCoordinatesOffsetX, rectCoordinatesOffsetY, text, points } = selectedElement;
 
-    if (type != 'pencil') {
+    if (type === 'image') {
         const width = x2 - x1;
         const height = y2 - y1;
 
-        updateElement(id, event.clientX - offSetX, event.clientY - offSetY, event.clientX - offSetX + width, event.clientY - offSetY + height, type, { text: text });
-    } else {
-
-
-        const newPoints = [];
-        selectedElement.points.forEach((_, index) => {
-
-            newPoints.push({
-                x: event.clientX - selectedElement.offSetX[index]
-
-                , y: event.clientY - selectedElement.offSetY[index]
-            })
-        });
-
-        let tempNewArray = [...elements];
-
-
-        const width = x2 - x1;
-        const height = y2 - y1;
+        const tempNewArray = [...elements];
         const newId = parseInt(id.split("#")[1]);
-
 
         tempNewArray[newId] = {
             ...tempNewArray[newId],
-            points: newPoints, x1: event.clientX - rectCoordinatesOffsetX, y1: event.clientY - rectCoordinatesOffsetY, x2: event.clientX - rectCoordinatesOffsetX + width, y2: event.clientY - rectCoordinatesOffsetY + height
+            x1: event.clientX - offSetX,
+            y1: event.clientY - offSetY,
+            x2: event.clientX - offSetX + width,
+            y2: event.clientY - offSetY + height
         };
 
-        store.dispatch(setElement([tempNewArray, true,id.split("#")[0]]));
-        const roomId =   GlobalProps.room;
-  if(roomId != null) {
-    tempNewArray = tempNewArray[newId];
-    const key = id.split("#")[0];
-    GlobalProps.socket.emit("render-elements", { tempNewArray, roomId,key });
-  }
-    }
+        store.dispatch(setElement([tempNewArray, true, id.split("#")[0]]));
 
+        const roomId = GlobalProps.room;
+        if (roomId) {
+            const updatedElement = tempNewArray[newId];
+            const key = id.split("#")[0];
+            GlobalProps.socket.emit("render-elements", { updatedElement, roomId, key });
+        }
+    } else {
+        if (type !== 'pencil') {
+            const width = x2 - x1;
+            const height = y2 - y1;
+            updateElement(id, event.clientX - offSetX, event.clientY - offSetY, event.clientX - offSetX + width, event.clientY - offSetY + height, type, { text: text });
+        } else {
+            const newPoints = points.map((point, index) => ({
+                x: event.clientX - offSetX[index],
+                y: event.clientY - offSetY[index]
+            }));
+
+            const tempNewArray = [...elements];
+            const width = x2 - x1;
+            const height = y2 - y1;
+            const newId = parseInt(id.split("#")[1]);
+
+            tempNewArray[newId] = {
+                ...tempNewArray[newId],
+                points: newPoints,
+                x1: event.clientX - rectCoordinatesOffsetX,
+                y1: event.clientY - rectCoordinatesOffsetY,
+                x2: event.clientX - rectCoordinatesOffsetX + width,
+                y2: event.clientY - rectCoordinatesOffsetY + height
+            };
+
+            store.dispatch(setElement([tempNewArray, true, id.split("#")[0]]));
+
+            const roomId = GlobalProps.room;
+            if (roomId) {
+                const updatedElement = tempNewArray[newId];
+                const key = id.split("#")[0];
+                GlobalProps.socket.emit("render-elements", { updatedElement, roomId, key });
+            }
+        }
+    }
 }
